abfa8961b490092e55b62bbe06b4c32ca9805119
WhatCats <whatcater@gmail.com>
2023-04-23 17:40:57 +0200

    Added async methods for listen/unlisten and more examples

--- README.md
+++ README.md
@@ -6,29 +6,32 @@ PGClient wrapper for triggering and listening to notification.
 > See more about Postgres notifications here: https://www.postgresql.org/docs/15/libpq-notify.html
 
 ## Disconnects
-Disconnects could occur with the database if network issues occur or if the database process is restarted.
-In the case of a disconnect this will <ins><b>automatically try reconnecting until it is successful or destrpyed</b></ins> with a 5 second cooldown.
+Network issues could occur or the database process may get restarted while your application is running.
+In the case of a disconnect this will <ins><b>automatically try reconnecting until it is successful or destroyed</b></ins> with a 5 second cooldown.
+While the client is disconnected any `.query()` call including `.notify()` will throw an error.
 Once reconnected it will automatically start listening to all of the channels you added listeners for again.
 To see the reconnect process in action, add a listener to the debug event like shown in the example below.
 
-## Example
+## Basic Example
 ```js
 // Same constructor as Client form pg but defaults to process.env.POSTGRES_CONN_URI
 const ipc = new PostgresIPCClient(process.env.POSTGRES_CONN_URI)
 ipc.on("debug", console.log) // optional debug content if needed
 ipc.on("error", console.error) // This is recommended. All uncaught errors from your (async) listeners will end up here.
+ipc.on("end", () => console.log("This is called when destroy is called if the client was previously connected."))
 
-// You can add listeners before connecting but they won't get called until you connect
+// You can add listeners before connecting.
 ipc.on("channel-name", (notification) => {
-    if (notification.processId === ipc.processId) return // ignore notifications sent by this client like this
+    if (notification.processId === ipc.processId) return // Ignore notifications sent by this client like this.
     console.log(notification.payload.message)
 })
+// Once connected it will auto query 'LISTEN channel-name' since you added this listener here.
 
 ipc.connect().then(async () => {
-    // this returns a promise but it does not throw errors so you can choose not to await this safely
+    // this returns a promise but it does not throw errors so you can choose not to await this safely.
     // returns Promise<undefined> if successful and Promise<Error> if error
-    // (the error is already sent to console.error so no need to worry about logging the error)
-    await ipc.notify("channel-name", { message: "hello" })
+    // (The error is already sent to console.error so no need to worry about logging the error.)
+    ipc.notify("channel-name", { message: "hello" })
 
     // once destroyed you can't connect again
     await ipc.destroy()
@@ -36,8 +39,34 @@ ipc.connect().then(async () => {
     await ipc.destroy()
 })
 
-ipc.notify("channel-name", { message: "hello" }) // Calling notify before connecting will result in an error!!!
-ipc.connect() // you can call connect as many times as you want without errors
+// Calling notify before connecting will wait for the PG-Client to connect and make the notify query before resolving the result promise.
+// If the client was destroyed or got disconnected this will throw an error.
+ipc.notify("channel-name", { message: "hello" }) 
+ipc.connect() // You can call connect as many times as you want without errors.
+```
+
+## Using async methods to listen & unlisten
+```js
+const ipc = new PostgresIPCClient()
+ipc.on("error", console.error)
+ipc.on("notify", (event, payload) => console.log("notify called", event, payload))
+ipc.on("notification", (notification) => {
+    if (notification.channel === "channel") console.log(notification.payload)
+})
+
+ipc.connect().then(async () => {
+    await ipc.listen("channel")
+    await ipc.listen(["channel1", "channel2"])
+    
+    await ipc.unlisten("channel2")
+    console.log(ipc.channels()) // These are the channels you are currently listening for.
+
+    // This will log 'notify called channel Hello!' bcs of the .on("notify") listener above.
+    // This will also log 'Hello!' bcs of the .on("notification") listener above.
+    await ipc.notify("channel", "Hello!")
+
+    await ipc.destroy()
+})
 ```
 
 ## Credits
--- src/IPC.ts
+++ src/IPC.ts
@@ -2,17 +2,7 @@ import { ClientConfig, Notification, Client as PGClient } from "pg"
 import { EventEmitter } from "events"
 
 const sleep = (seconds: number) => new Promise((r) => setTimeout(r, seconds * 1000))
-const RESERVED_CHANNELS = [
-    "newListener",
-    "removeListener",
-    "notification",
-    "unlisten",
-    "notify",
-    "listen",
-    "error",
-    "debug",
-    "end"
-]
+const RESERVED_CHANNELS = ["newListener", "removeListener", "notification", "notify", "error", "debug", "end"]
 
 type Status = "initial" | "connecting" | "reconnecting" | "connected" | "ending" | "terminated" | "dead"
 
@@ -20,6 +10,7 @@ class PostgresIPCClient extends EventEmitter {
     protected config: ClientConfig
     protected _client!: PGClient
     protected _status: Status = "initial"
+    protected _channels: Set<string> = new Set()
 
     constructor(config: string | ClientConfig = {}) {
         super({ captureRejections: true })
@@ -31,19 +22,21 @@ class PostgresIPCClient extends EventEmitter {
         this.config = config
         this.createClient()
 
-        const dispatch = (channel: string) => {
+        const dispatch = (channel: string | symbol): channel is string => {
             return (
-                this._status === "connected" &&
-                !RESERVED_CHANNELS.includes(channel) &&
-                this.listenerCount(channel) === 0
+                this.status === "connected" &&
+                typeof channel === "string" &&
+                !RESERVED_CHANNELS.includes(channel)
             )
         }
 
-        this.on("newListener", (channel: string) => {
-            if (dispatch(channel)) this.dispatchListen(channel)
+        this.on("newListener", (channel: string | symbol) => {
+            if (dispatch(channel) && !this._channels.has(channel)) this.listen(channel).catch(console.error)
         })
-        this.on("removeListener", (channel: string) => {
-            if (dispatch(channel)) this.dispatchUnlisten(channel)
+
+        this.on("removeListener", (channel: string | symbol) => {
+            if (dispatch(channel) && this.listenerCount(channel) === 0 && this._channels.has(channel))
+                this.unlisten(channel).catch(console.error)
         })
     }
 
@@ -56,28 +49,120 @@ class PostgresIPCClient extends EventEmitter {
     }
 
     get processId() {
-        return (this._client as any).processID as number | null
+        return ((this.client as any)?.processID ?? null) as number | null
+    }
+
+    /** All PG channels you are currently listening for */
+    channels() {
+        return this.eventNames()
+            .concat(...this._channels)
+            .filter((v) => typeof v === "string" && !RESERVED_CHANNELS.includes(v)) as string[]
     }
 
     async connect() {
-        if (["initial", "reconnecting"].includes(this._status)) {
-            if (this._status === "initial") this._status = "connecting"
-            await this._client.connect()
+        if (this.status === "initial" || this.status === "reconnecting") {
+            if (this.status === "initial") this._status = "connecting"
+            await this.client.connect()
             this._status = "connected"
             this.emit("debug", `PG-IPC Connected`)
-            await this.addListens()
+            await this.listen(this.channels()).catch(console.error)
         }
     }
 
-    protected async addListens() {
-        const channels = this.eventNames().filter((v) => !RESERVED_CHANNELS.includes(v as string))
-        this.emit("debug", `PG-IPC adding listeners for channels [${channels.join(", ")}]`)
-        const query = channels.map((channel) => `LISTEN ${this._client.escapeIdentifier(channel as string)}`)
-        await this._client.query(query.join(";")).catch(console.error)
+    async destroy() {
+        const status = this._status
+        if (status === "connected" || status === "reconnecting") {
+            this._status = "ending"
+            if (status === "connected") await this.unlisten().catch(console.error)
+            this.client.removeAllListeners()
+            this.emit("end")
+            this.emit("debug", `PG-IPC Closing after destroy was called`)
+            this.removeAllListeners()
+            await this.client.end().catch(console.error)
+            this._status = "terminated"
+        }
+    }
+
+    /**
+     * @param payload non-string data will be auto encoded to JSON -
+     * In the default configuration it must be shorter than 8000 bytes.
+     * (If binary data or large amounts of information need to be communicated, it's best to put it in a database table and send the key of the record.)
+     * @returns error if there was one after calling console.error
+     */
+    async notify(channel: string, payload: any = null) {
+        const encoded = (
+            typeof payload === "string" || payload === null ? payload : JSON.stringify(payload)
+        ) as string
+        const res = await this.query(`SELECT pg_notify($1, $2)`, [channel, encoded])
+            .then(() => this.emit("debug", `PG-IPC Message sent over '${channel}' channel`))
+            .then(() => this.emit("notify", channel, payload))
+            .catch((err) => {
+                console.error(err)
+                return err
+            })
+
+        if (res instanceof Error) return res
+    }
+
+    async listen(channelOrChannels: string | string[]) {
+        const channels = (
+            typeof channelOrChannels === "string" ? [channelOrChannels] : channelOrChannels
+        ).filter((channel) => !RESERVED_CHANNELS.includes(channel))
+        const query = channels.map((channel) => `LISTEN ${this.client.escapeIdentifier(channel)}`)
+        if (query.length > 0) {
+            await this.query(query.join(";"))
+            channels.forEach((channel) => this._channels.add(channel))
+            this.emit("debug", `PG-IPC ran listen for channels [${channels.join(", ")}]`)
+        }
+    }
+
+    /**
+     * This will also remove any listeners added for `channelOrChannels`
+     * @param channelOrChannels leave undefined to unlisten from all channels
+     */
+    async unlisten(channelOrChannels?: string | string[]) {
+        const channels = typeof channelOrChannels === "string" ? [channelOrChannels] : channelOrChannels
+        if (channels === undefined) {
+            await this.query("UNLISTEN *")
+            this._channels.forEach((channel) => {
+                this._channels.delete(channel)
+                this.removeAllListeners(channel)
+            })
+            this.emit("debug", `PG-IPC ran unlisten for all channels`)
+        } else {
+            const remove = channels.filter((channel) => !RESERVED_CHANNELS.includes(channel))
+            const query = remove.map((channel) => `UNLISTEN ${this.client.escapeIdentifier(channel)}`)
+            if (query.length > 0) {
+                await this.query(query.join(";"))
+                remove.forEach((channel) => this._channels.delete(channel))
+                channels.forEach((channel) => this.removeAllListeners(channel))
+                this.emit("debug", `PG-IPC ran unlisten for channels [${remove.join(", ")}]`)
+            }
+        }
+    }
+
+    async query(query: string, params?: any[]) {
+        if (this.status === "dead" || this.status === "reconnecting")
+            throw Error(`PG-IPC-Client has encountered a connection error and is currently not queryable.`)
+        if (this.status === "terminated") throw Error(`PG-IPC-Client was shutdown and is not queryable.`)
+        return this.client.query(query, params)
+    }
+
+    protected createClient() {
+        if (this.client) this.client.removeAllListeners()
+        this._client = new PGClient(this.config)
+        this._client.on("notification", (msg) => this.onNotification(msg).catch(console.error))
+        this._client.on("error", (error) => {
+            if (this._status === "connected") {
+                this._status = "dead"
+                this.emit("debug", `PG-IPC connection terminated unexpectedly! (${error})`)
+                this.reconnect()
+            }
+        })
     }
 
     protected async reconnect() {
-        if (this._status === "dead") {
+        if (this.status === "dead") {
             this._status = "reconnecting"
             this.emit("debug", `PG-IPC attempting reconnect after 5 seconds...`)
             await sleep(5)
@@ -88,51 +173,8 @@ class PostgresIPCClient extends EventEmitter {
 
     protected async reconnectError(err: Error) {
         this._status = "dead"
-        this.emit("debug", `PG-IPC failed to reconnect! ${err}`)
+        this.emit("debug", `PG-IPC failed to reconnect! (${err})`)
         this.reconnect()
-    }
-
-    async destroy() {
-        const status = this._status
-        if (status === "connected" || status === "reconnecting") {
-            this._status = "ending"
-            if (status === "connected") await this._client.query(`UNLISTEN *`).catch(console.error)
-            this._client.removeAllListeners()
-            this.emit("end")
-            this.removeAllListeners()
-            await this._client.end().catch(console.error)
-            this._status = "terminated"
-            this.emit("debug", `PG-IPC Terminated`)
-        }
-    }
-
-    protected createClient() {
-        if (this._client) this._client.removeAllListeners()
-        this._client = new PGClient(this.config)
-        this._client.on("notification", (msg) => this.onNotification(msg).catch(console.error))
-        this._client.on("error", (error) => {
-            if (this._status === "connected") {
-                this._status = "dead"
-                this.emit("debug", `PG-IPC connection terminated unexpectedly! ${error}`)
-                this.reconnect()
-            }
-        })
-    }
-
-    /**
-     * @returns error if there was one
-     */
-    async notify(channel: string, payload: any = null) {
-        const encoded = typeof payload === "string" || payload === null ? payload : JSON.stringify(payload)
-        const res = await this._client
-            .query(`SELECT pg_notify($1, $2)`, [channel, encoded])
-            .then(() => this.emit("notify", channel, payload))
-            .catch((err) => {
-                console.error(err)
-                return err
-            })
-
-        if (res instanceof Error) return res
     }
 
     protected async onNotification(msg: Notification) {
@@ -145,18 +187,6 @@ class PostgresIPCClient extends EventEmitter {
             this.emit("notification", msg as PGNotification)
             this.emit(msg.channel, msg as PGNotification)
         }
-    }
-
-    protected async dispatchListen(channel: string) {
-        await this._client
-            .query(`LISTEN ${this._client.escapeIdentifier(channel)}`)
-            .then(() => this.emit("listen", channel))
-    }
-
-    protected async dispatchUnlisten(channel: string) {
-        await this._client
-            .query(`UNLISTEN ${this._client.escapeIdentifier(channel)}`)
-            .then(() => this.emit("unlisten", channel))
     }
 
     emit(event: string, notification: PGNotification): boolean
@@ -186,6 +216,7 @@ class PostgresIPCClient extends EventEmitter {
 
 interface PGNotification extends Notification {
     length: number
+    /** JSON payload will be automatically parsed as such */
     payload: any
 }
 
@@ -194,8 +225,6 @@ interface ThisEvents {
     removeListener: [channel: string]
     notification: [notification: PGNotification]
     notify: [channel: string, payload: any]
-    unlisten: [channel: string]
-    listen: [channel: string]
     debug: [message: string]
     error: [error: any]
     end: []
